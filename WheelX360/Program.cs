2a7a4cee29a9bbe5145fc3dd3b5a79ac5add8417
Soma Nyiro <nysoma@protonmail.com>
2024-02-22 17:37:25 +0100

    setting up project
    now they both build to the same folder

--- WheelX360/Program.cs
+++ WheelX360/Program.cs
@@ -1,2 +1,16 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Reflection;
+
+string currentAssemblyPath = Assembly.GetExecutingAssembly().Location;
+string currentDirectory = Path.GetDirectoryName(currentAssemblyPath);
+
+string forceFeedback = Path.Combine(currentDirectory, "WheelForceFeedback.exe");
+
+ProcessStartInfo startInfo = new ProcessStartInfo(forceFeedback);
+#if !DEBUG
+startInfo.CreateNoWindow = true;
+startInfo.UseShellExecute = false;
+#endif
+Process.Start(startInfo);
+
+Console.WriteLine("Hello, World! 1");
