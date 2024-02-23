46d33bd2c7f26df46bcf276c6bb127a91bb31913
Soma Nyiro <nysoma@protonmail.com>
2024-02-23 13:59:31 +0100

    setting up communication -created shared class library -added named pipe wrapper package

---
+++ SharedClasses/Messages.cs
@@ -0,0 +1,17 @@
+﻿namespace SharedClasses;
+
+[Serializable]
+public class ActivateRumbleMessage
+{
+    public byte SmallMotor { get; set; }
+    public byte LargeMotor { get; set; }
+}
+
+[Serializable]
+public class FeedbackSettings
+{
+    public bool RumbleEnabled { get; set; }
+    public float RumbleForce { get; set; }
+    public bool CenterSpringEnabled { get; set; }
+    public float centerSpringForce { get; set; }
+}
