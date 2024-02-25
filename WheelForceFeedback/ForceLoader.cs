5c71b8dca2bdadd3415f2777d7348208dfee021f
Soma Nyiro <nysoma@protonmail.com>
2024-02-25 23:52:47 +0100

    replaced center spring with actual spring effect

--- WheelForceFeedback/ForceLoader.cs
+++ WheelForceFeedback/ForceLoader.cs
@@ -10,9 +10,8 @@ namespace WheelForceFeedback;
 
 public class ForceLoader
 {
-    private ConstantForceEffect turnWheelLeft = new ConstantForceEffect();
-    private ConstantForceEffect turnWheelRight = new ConstantForceEffect();
-    private PeriodicForceEffect rumbleWheel = new PeriodicForceEffect(PeriodicForceEffectKind.TriangleWave);
+    private PeriodicForceEffect rumbleWheel;
+    private ConditionForceEffect centerWheel;
     
     private FeedbackSettings feedbackSettings = new FeedbackSettings();
     
@@ -24,11 +23,6 @@ public class ForceLoader
 
         racingWheel.WheelMotor.TryResetAsync();
         LoadForceEffects();
-
-        Task.Run(async () =>
-        {
-            await CenterWheel();
-        });
         
         while (true)
         {
@@ -38,78 +32,38 @@ public class ForceLoader
             if (type == (int)MessageType.Rumble)
             {
                 ActivateRumbleMessage rumbleMessage = JsonSerializer.Deserialize<ActivateRumbleMessage>(message.Substring(1));
-                rumbleWheel.Stop();
                 rumbleWheel.Gain = feedbackSettings.RumbleForce * Utils.Map(
                     (int)rumbleMessage.LargeMotor + (int)rumbleMessage.SmallMotor,
                     0, 510, 0, 1);
-                rumbleWheel.Start();
             }
 
             if (type == (int)MessageType.Setting)
             {
                 feedbackSettings = JsonSerializer.Deserialize<FeedbackSettings>(message.Substring(1));
                 rumbleWheel.SetParameters(new(0.5f, 0, 0), feedbackSettings.RumbleFrequency, 0.5f, 0f, TimeSpan.MaxValue);
-                turnWheelLeft.Gain = feedbackSettings.CenterSpringForce;
-                turnWheelRight.Gain = feedbackSettings.CenterSpringForce;
-
+                
             }
             
             messageServer.SendFrame("message received");
         }
     }
 
-    async Task CenterWheel()
-    {
-        while (true)
-        {
-            if (racingWheel.GetCurrentReading().Wheel > feedbackSettings.CenterSpringDeadzone / 90)
-            {
-                if (turnWheelLeft.State != ForceFeedbackEffectState.Running)
-                    turnWheelLeft.Start();
-            }
-            else
-            {
-                if (turnWheelLeft.State == ForceFeedbackEffectState.Running)
-                    turnWheelLeft.Stop();
-            }
-
-            if (racingWheel.GetCurrentReading().Wheel < -(feedbackSettings.CenterSpringDeadzone / 90))
-            {
-                if (turnWheelRight.State != ForceFeedbackEffectState.Running)
-                    turnWheelRight.Start();
-            }
-            else
-            {
-                if (turnWheelRight.State == ForceFeedbackEffectState.Running)
-                    turnWheelRight.Stop();
-            }
-        }
-    }
-
     async void LoadForceEffects()
     {
-        turnWheelLeft = new();
-        turnWheelRight = new();
+        centerWheel = new(ConditionForceEffectKind.Spring);
         rumbleWheel = new(PeriodicForceEffectKind.TriangleWave);
-        turnWheelLeft.SetParameters(new(0.5f, 0, 0), TimeSpan.MaxValue);
-        turnWheelRight.SetParameters(new(-0.5f, 0, 0), TimeSpan.MaxValue);
+        centerWheel.SetParameters(new(0.5f, 0, 0), 1, 1, 1, 1, 0.1f, 0);
         rumbleWheel.SetParameters(new(0.5f, 0, 0), feedbackSettings.RumbleFrequency, 0.5f, 0f, TimeSpan.MaxValue);
+
+        IAsyncOperation<ForceFeedbackLoadEffectResult> loadCenterRequest =
+            racingWheel.WheelMotor.LoadEffectAsync(centerWheel);
+
+        ForceFeedbackLoadEffectResult result = await loadCenterRequest.AsTask();
         
-        IAsyncOperation<ForceFeedbackLoadEffectResult> loadLeftRequest =
-            racingWheel.WheelMotor.LoadEffectAsync(turnWheelLeft);
-
-        ForceFeedbackLoadEffectResult result = await loadLeftRequest.AsTask();
-
         if (result == ForceFeedbackLoadEffectResult.Succeeded)
-            Console.WriteLine("left effect loaded");
-
-        IAsyncOperation<ForceFeedbackLoadEffectResult> loadRightRequest =
-            racingWheel.WheelMotor.LoadEffectAsync(turnWheelRight);
-
-        result = await loadRightRequest.AsTask();
-
-        if (result == ForceFeedbackLoadEffectResult.Succeeded)
-            Console.WriteLine("right effect loaded");
+            Console.WriteLine("center effect loaded");
+        
+        centerWheel.Start();
 
         IAsyncOperation<ForceFeedbackLoadEffectResult> loadRumbleRequest =
             racingWheel.WheelMotor.LoadEffectAsync(rumbleWheel);
@@ -118,12 +72,14 @@ public class ForceLoader
 
         if (result == ForceFeedbackLoadEffectResult.Succeeded)
             Console.WriteLine("rumble effect loaded");
+
+        rumbleWheel.Gain = 0;
+        rumbleWheel.Start();
     }
 
     void UnLoadForceEffects()
     {
-        racingWheel.WheelMotor.TryUnloadEffectAsync(turnWheelLeft);
-        racingWheel.WheelMotor.TryUnloadEffectAsync(turnWheelRight);
+        racingWheel.WheelMotor.TryUnloadEffectAsync(centerWheel);
         racingWheel.WheelMotor.TryUnloadEffectAsync(rumbleWheel);
     }
 }
